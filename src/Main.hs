10d55d6c78641dd7da5499d3d41afcfc0b5047c8
Beat Hagenlocher <bah@posteo.de>
2024-03-21 22:22:27 +0100

    Add parseString

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,25 @@ module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 
+data LispVal = Atom String
+            | List [LispVal]
+            | DottedList [LispVal] LispVal
+            | Number Integer
+            | String String
+            | Bool Bool
+
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 spaces :: Parser ()
 spaces = skipMany1 space
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return (String x)
 
 readExpr :: String -> String
 readExpr input = case parse (spaces >> symbol) "lisp" input of
