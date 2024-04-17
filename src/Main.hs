07f5b54dd5a5cc166451737c8c28706a1bf374f2
Beat Hagenlocher <bah@posteo.de>
2024-04-17 23:38:26 +0200

    Add basic eval & apply

--- src/Main.hs
+++ src/Main.hs
@@ -17,14 +17,12 @@ data LispVal = Atom String
 instance Show LispVal where show = showVal
 
 main :: IO ()
-main = do
-  (expr:_) <- getArgs
-  putStrLn (readExpr expr)
+main = getArgs >>= print . eval . readExpr . head
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-  Left err -> "No match: " ++ show err
-  Right val -> show val
+  Left err -> String ("No match: " ++ show err)
+  Right val -> val
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
@@ -51,6 +49,39 @@ parseExprWithUnquote = parseAtom
                           x <- try parseList <|> parseDottedList
                           char ')'
                           return x
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val 
+eval val@(Number _) = val 
+eval val@(Bool _) = val 
+eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func (map eval args)
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) (lookup func primitives)
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [
+    ("+", numericBinop (+)),
+    ("-", numericBinop (-)),
+    ("*", numericBinop (*)),
+    ("/", numericBinop div),
+    ("mod", numericBinop mod),
+    ("quotient", numericBinop quot),
+    ("remainder", numericBinop rem)
+  ]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number (foldl1 op (map unpackNum params))
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n :: [(Integer, String)] in
+                           if null parsed
+                              then 0
+                              else fst (head parsed)
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
 
 showVal :: LispVal -> String
 showVal (String contents) = "\"" ++ contents ++ "\""
