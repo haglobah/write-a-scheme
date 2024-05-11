f1f3335963187ffbc5607d0af58aa83692222904
Beat Hagenlocher <bah@posteo.de>
2024-05-11 16:10:20 +0200

    Add car, cdr, cons and eqv

--- src/Main.hs
+++ src/Main.hs
@@ -97,6 +97,40 @@ apply func args = maybe (throwError (NotFunction "Unrecognized primitive functio
                         ($ args)
                         (lookup func primitives)
 
+car :: [LispVal] -> ThrowsError LispVal
+car [List (x : _xs)]         = return x
+car [DottedList (x : _xs) _] = return x
+car [badArg]               = throwError (TypeMismatch "pair" [badArg])
+car badArgList             = throwError (NumArgs 1 badArgList)
+
+cdr :: [LispVal] -> ThrowsError LispVal
+cdr [List (_x : xs)]        = return (List xs)
+cdr [DottedList [_] x]      = return x
+cdr [DottedList (_ : xs) x] = return (DottedList xs x)
+cdr [badArg]                = throwError (TypeMismatch "pair" [badArg])
+cdr badArgList              = throwError (NumArgs 1 badArgList)
+
+cons :: [LispVal] -> ThrowsError LispVal
+cons [x, List []] = return (List [x])
+cons [x, List xs] = return (List (x : xs))
+cons [x, DottedList xs xlast] = return (DottedList (x : xs) xlast)
+cons [x1, x2] = return (DottedList [x1] x2)
+cons badArgList = throwError (NumArgs 2 badArgList)
+
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [(Bool arg1), (Bool arg2)]             = return $ Bool $ arg1 == arg2
+eqv [(Number arg1), (Number arg2)]         = return $ Bool $ arg1 == arg2
+eqv [(String arg1), (String arg2)]         = return $ Bool $ arg1 == arg2
+eqv [(Atom arg1), (Atom arg2)]             = return $ Bool $ arg1 == arg2
+eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
+eqv [(List arg1), (List arg2)]             = return $ Bool $ (length arg1 == length arg2) && 
+                                                             (all eqvPair $ zip arg1 arg2)
+     where eqvPair (x1, x2) = case eqv [x1, x2] of
+                                Left err -> False
+                                Right (Bool val) -> val
+eqv [_, _]                                 = return $ Bool False
+eqv badArgList                             = throwError $ NumArgs 2 badArgList
+
 primitives :: [(String, [LispVal] -> ThrowsError LispVal)]
 primitives = [
     ("+", numericBinop (+)),
